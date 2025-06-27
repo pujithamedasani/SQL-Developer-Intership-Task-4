3573af3c773bfe2784334cd067df78bd836136aa
Pujitha Medasani <mpujitha676@gmail.com>
2025-06-27 18:26:44 +0530

    aggregate_functions.sql

--- aggregate_functions.sql
+++ aggregate_functions.sql
@@ -6,11 +6,15 @@ DML Commands are SELECT , INSERT , UPDATE , DELETE .
 Aggregate Functions: 
 Functions that perform a calculation on a group of rows and returns a single result.
 
-For example: total , average , count
-
+For example: total , average , count 
+  
+These are multiple row functions which is also known as Group functions which work with data of multiple rows at a time and return aggregated value.
+  
+  examples: sum() , min() , max() , Avg() , count() 
+  
 Syntax: SELECT AGG_FUNC(column_name) FROM table_name;
 
-Example :
+SQL script :
 
 -- Total number of members
 SELECT COUNT(*) FROM Members;  
